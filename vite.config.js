f37b591e74309cbbc4e3701350fb148c608c9547
ManOnFireEverytime <55137858+ManOnFireEverytime@users.noreply.github.com>
2023-03-06 05:53:08 +0000

    vitee

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,9 @@ import vue2 from '@vitejs/plugin-vue2'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/cw3ind/',
+  build: {
+    chunkSizeWarningLimit: 100000000
+},
   plugins: [
     vue2(),
     legacy({
